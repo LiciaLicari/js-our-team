bdaa6f9ea02171d1a432888895c4ab1387900cc3
“LiciaLicari” <licianbsplicari@gmail.com>
2023-09-06 16:05:23 +0200

    add MS 2
    * need fix: pics not showing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,91 +1,126 @@
 console.log("miao");
 
 /*
-Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. 
-Ogni membro è caratterizzato dalle seguenti informazioni: 
-nome, ruolo e foto.
+    Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. 
+    Ogni membro è caratterizzato dalle seguenti informazioni: 
+    nome, ruolo e foto.
 
-MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
-MILESTONE 1:
-Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
-MILESTONE 2:
-Stampare le stesse informazioni su DOM sottoforma di stringhe
-BONUS 1:
-Trasformare la stringa foto in una immagine effettiva
-BONUS 2:
-Organizzare i singoli membri in card/schede
+    MILESTONE 0:
+    Creare l’array di oggetti con le informazioni fornite.
+    MILESTONE 1:
+    Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+    MILESTONE 2:
+    Stampare le stesse informazioni su DOM sottoforma di stringhe
+    BONUS 1:
+    Trasformare la stringa foto in una immagine effettiva
+    BONUS 2:
+    Organizzare i singoli membri in card/schede
 
 
-Dati:
-Wayne Barnett
-Founder & CEO	
-wayne-barnett-founder-ceo.jpg
-Angela Caroll	
-Chief Editor	
-angela-caroll-chief-editor.jpg
-Walter Gordon
-Office Manager
-walter-gordon-office-manager.jpg
-Angela Lopez
-Social Media Manager	
-angela-lopez-social-media-manager.jpg
-Scott Estrada	
-Developer	
-scott-estrada-developer.jpg
-Barbara Ramos	
-Graphic Designer	
-barbara-ramos-graphic-designer.jpg
+    Dati:
+    Wayne Barnett
+    Founder & CEO	
+    wayne-barnett-founder-ceo.jpg
+    Angela Caroll	
+    Chief Editor	
+    angela-caroll-chief-editor.jpg
+    Walter Gordon
+    Office Manager
+    walter-gordon-office-manager.jpg
+    Angela Lopez
+    Social Media Manager	
+    angela-lopez-social-media-manager.jpg
+    Scott Estrada	
+    Developer	
+    scott-estrada-developer.jpg
+    Barbara Ramos	
+    Graphic Designer	
+    barbara-ramos-graphic-designer.jpg
 */
 
 /* 
-MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite. 
+    MILESTONE 0:
+    Creare l’array di oggetti con le informazioni fornite. 
 */
 
 const team = [
     {
         fullName: 'Wayne Barnett',
         position: 'Founder & CEO',
-        image: './assets/img/wayne-barnett-founder-ceo.jpg'  
+        image: './assets/img/wayne-barnett-founder-ceo.jpg'
     },
     {
         fullName: 'Angela Caroll',
         position: 'Chief Editor',
-        image: './assets/img/angela-lopez-social-media-manager.jpg'  
+        image: './assets/img/angela-lopez-social-media-manager.jpg'
     },
     {
         fullName: 'Walter Gordon',
         position: 'Office Manager',
-        image: './assets/img/walter-gordon-ofice-manager.jpg'  
+        image: './assets/img/walter-gordon-ofice-manager.jpg'
     },
     {
         fullName: 'Angela Lopez',
         position: 'Social Media Manager',
-        image: './assets/img/angela-lopez-social-media-manager.jpg'  
+        image: './assets/img/angela-lopez-social-media-manager.jpg'
     },
     {
         fullName: 'Scott Estrada',
         position: 'Developer',
-        image: './assets/img/scott-estrada-developer.jpg'  
+        image: './assets/img/scott-estrada-developer.jpg'
     },
     {
         fullName: 'Barbara Ramos',
         position: 'Graphic Designer',
-        image: './assets/img/barbara-ramos-graphic-designer.jpg'  
+        image: './assets/img/barbara-ramos-graphic-designer.jpg'
     },
 ]
 
 /*
-MILESTONE 1:
-Stampare su console, per ogni membro del team, le informazioni di 
-nome, ruolo e la stringa della foto
+    MILESTONE 1:
+    Stampare su console, per ogni membro del team, le informazioni di 
+    nome, ruolo e la stringa della foto
 */
 
 for (let i = 0; i < team.length; i++) {
     const member = team[i];
-    
+
     for (const key in member) {
         console.log(`key - ${key} : value - ${member[key]}`);
     }
-} 
+}
+
+/*
+    MILESTONE 2:
+    Stampare le stesse informazioni su DOM sottoforma di stringhe
+*/
+
+
+const teamMembersElement = document.querySelector('.row');
+
+for (let i = 0; i < team.length; i++) {
+    const member = team[i];
+
+
+    const memberMarkup = `<div class="col-4">
+        <div class="card my-3">
+            <div class="card-img">
+            <img class="img-fluid" src="${member.img}" alt>
+            </div>
+            <div class="card-body">
+                <h4>Nome: ${member.fullName}</h4>
+                <h5>Ruolo: ${member.position}</h5>
+            </div>
+        </div>
+    </div>`
+
+    teamMembersElement.insertAdjacentHTML('beforeend', memberMarkup);
+
+    console.log(member);
+
+    for (const key in member) {
+
+        console.log(key, member[key]);
+    }
+
+}
